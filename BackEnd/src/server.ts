7b431ad7c5a8790bf505d13c9762ab749f2d3b7a
Omar Yasser <anaesthesiayassermadi717@gmail.com>
2025-05-22 01:35:06 +0300

    Backend/installation

---
+++ BackEnd/src/server.ts
@@ -0,0 +1,13 @@
+import express from 'express';
+const app = express();
+const PORT = 5000;
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.send('Backend server is running!');
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
